1c0c7e41a702fb966aab2dd0cb2c30535c5da5fe
MayarSultanAlanaze <126346292+MayarSultanAlanaze@users.noreply.github.com>
2023-04-06 08:46:18 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,35 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        int[] Number = {50, 60, 40, 10, 38, 65, 87, 56, 90, 100, 66, 80, 88, 40, 98};
+
+        for (int n : Number) {
+            if (n >=95) {
+                System.out.println( n+ "The Gread +A");
+            } else if (n>=90) {
+                System.out.println(n+"The Gread A");
+            } else if (n>=85) {
+                System.out.println( n+ "The Gread +B");
+            } else if (n>=80) {
+                System.out.println( n+ "The Gread B");
+            } else if(n>=75) {
+                System.out.println( n+ "The Gread +C");
+            } else if (n>=70) {
+                System.out.println( n+ "The Gread C");
+            } else if(n>=65) {
+                System.out.println( n+ "The Gread +D");
+            } else if (n>=60) {
+                    System.out.println( n+ "The Gread D");
+            } else {
+                System.out.println( n+ "The Gread F");
+
+            }
+
+
+        }
+
+        }
+
+        }
+
